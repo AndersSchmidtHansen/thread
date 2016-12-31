2bb3422d33f1319970b921d59feedd02f19884d0
Anders Schmidt Hansen <me@andersschmidt.com>
2016-12-31 17:17:30 +0100

    -

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/public/',
+    path: path.resolve(__dirname, './public/dist'),
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
